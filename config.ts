92e7205a3ae3b4516f5cb8dc75e18ca0cb2e60e5
Taheraldev <101641519+Taheraldev@users.noreply.github.com>
2023-03-10 20:50:20 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,28 +1,50 @@
 const config: Config = {
-    mode: 'CHINA', // 'CHINA' | 'WORLD'
+    // AI_PAINTING_SPRING
+    // DIFFERENT_DIMENSION_ME
+    // AI_PAINTING_ANIME
+    // AIGCSDK_AI_PAINTING_ANIME
+    mode: 'AI_PAINTING_SPRING',
+
     botToken: '5264968049:AAHUniq68Nqq39CrFf8lVqerwetirQnGxzc',
+
     keepFiles: {
         compared: true,
         input: true,
         single: true,
-        video: false,
+        video: true,
     },
+
     messages: {
-        blocked: 'The Chinese server has blocked the bot, too bad',
+        blocked: 'The Chinese website has blocked the bot, too bad 🤷‍♂️',
         bye:
-            'ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ᴛʜɪꜱ ʙᴏᴛ 🥰\n' +
-            '@MikaEirene ᴊᴏɪɴ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ',
+            'Thank you for using this bot 👍\n' +
+            'Please rate and fork it on [Github](https://github.com/lmcsu/qq-neural-anime-tg) ♥️',
         hello: 'Hi 👋 Send me a photo to convert it into a 2D anime art',
-        media: '@MikaEirene\\_bot',
+        media: '@qq\\_neural\\_anime\\_bot',
         received: 'Photo has been received, please wait',
     },
+
     sendMedia: {
+        // AI_PAINTING_SPRING
+        // DIFFERENT_DIMENSION_ME
+        // AI_PAINTING_ANIME
         compared: true,
-        single: true, // works only with mode=CHINA
-        video: false, // works only with mode=CHINA
+
+        // AI_PAINTING_SPRING
+        // DIFFERENT_DIMENSION_ME
+        // AI_PAINTING_ANIME
+        // AIGCSDK_AI_PAINTING_ANIME
+        single: true,
+
+        // AI_PAINTING_SPRING
+        // AI_PAINTING_ANIME
+        video: true,
     },
-   // proxyUrl: 'socks5://@58.49.230.248:30001',
+
+    parallelRequests: 10,
+
+    // Uncomment the line below and set your proxy if you need it.
+    // proxyUrl: 'socks5://@58.49.230.248:30001',
 };
 
 export default config;
-    
